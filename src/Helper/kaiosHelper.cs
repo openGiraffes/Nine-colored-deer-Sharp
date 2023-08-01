89e76f022f726bb670d7cf4850949a8f5b5fc720
zixing <1311817771@qq.com>
2023-08-01 18:33:50 +0800

    初始版本
    初始版本

---
+++ src/Helper/kaiosHelper.cs
@@ -0,0 +1,160 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Nine_colored_deer_Sharp.Helper
+{
+    public class kaiosHelper
+    {
+        Socket tcpClient;
+
+        public byte[] withLen(string data)
+        {
+            data = data.Length + ":" + data;
+            return Encoding.UTF8.GetBytes(data);
+        }
+
+        public kaiosHelper(string host = "127.0.0.1", int port = 6000)
+        {
+            tcpClient = new Socket(SocketType.Stream, ProtocolType.IP);
+            tcpClient.Connect(host, port);
+
+
+            List<byte> bytes = new List<byte>();
+
+            byte[] buffer = new byte[1] { 0 };
+
+            while (buffer[0] != 58)
+            {
+                tcpClient.Receive(buffer, 1, SocketFlags.None);
+                if (buffer[0] != 58)
+                {
+                    bytes.Add(buffer[0]);
+                }
+            }
+
+            var size = int.Parse(Encoding.UTF8.GetString(bytes.ToArray()));
+
+            bytes = new List<byte>();
+
+            buffer = new byte[1024];
+
+            while (bytes.Count < size)
+            {
+                var readcount = tcpClient.Receive(buffer, buffer.Length, SocketFlags.None);
+
+                bytes.AddRange(buffer.Take(readcount).ToArray());
+            }
+
+            tcpClient.Send(withLen(listTabCmd));
+
+            Thread.Sleep(100);
+
+            bytes = new List<byte>();
+
+            buffer = new byte[1];
+
+            while (buffer[0] != 58)
+            {
+                var ret = tcpClient.Receive(buffer, 1, SocketFlags.None);
+
+                if (buffer[0] != 58)
+                {
+                    bytes.Add(buffer[0]);
+                }
+            }
+
+            size = int.Parse(Encoding.UTF8.GetString(bytes.ToArray()));
+
+            buffer = new byte[size];
+
+            tcpClient.Receive(buffer, size, SocketFlags.None);
+
+            var jsondata = Encoding.UTF8.GetString(buffer);
+
+            deviceActor = JObject.Parse(jsondata)["deviceActor"].ToString();
+
+        }
+        string deviceActor;
+        string screenshotCmd = "{{\"type\":\"screenshotToDataURL\",\"to\":\"{0}\"}}";
+        string listTabCmd = "{\"to\":\"root\",\"type\":\"listTabs\"}";
+        string substring_cmd = "{{\"type\":\"substring\",\"start\":{0},\"end\":{1},\"to\":\"{2}\"}}";
+
+
+        public byte[] getImage()
+        {
+            string cmd = string.Format(screenshotCmd, deviceActor);
+            tcpClient.Send(withLen(cmd));
+
+
+            var bytes = new List<byte>();
+
+            var buffer = new byte[1];
+
+            while (buffer[0] != 58)
+            {
+                tcpClient.Receive(buffer, 1, SocketFlags.None);
+                if (buffer[0] != 58)
+                {
+                    bytes.Add(buffer[0]);
+                }
+            }
+            int size = int.Parse(Encoding.UTF8.GetString(bytes.ToArray()));
+
+
+            buffer = new byte[size];
+
+            tcpClient.Receive(buffer, size, SocketFlags.None);
+
+            var jsondata = Encoding.UTF8.GetString(buffer);
+            var image = JObject.Parse(jsondata)["value"];
+
+            var image_len = int.Parse(image["length"].ToString());
+
+            var actor = image["actor"].ToString();
+
+            cmd = string.Format(substring_cmd, 0, image_len, actor);
+
+            tcpClient.Send(withLen(cmd));
+
+
+            bytes = new List<byte>();
+
+            buffer = new byte[1];
+
+            while (buffer[0] != 58)
+            {
+                tcpClient.Receive(buffer, 1, SocketFlags.None);
+                if (buffer[0] != 58)
+                {
+                    bytes.Add(buffer[0]);
+                }
+            }
+            size = int.Parse(Encoding.UTF8.GetString(bytes.ToArray()));
+
+
+            bytes = new List<byte>();
+
+            buffer = new byte[1024 * 1024];
+
+            while (bytes.Count < size)
+            {
+                var readcount = tcpClient.Receive(buffer, buffer.Length, SocketFlags.None);
+
+                bytes.AddRange(buffer.Take(readcount).ToArray());
+            }
+
+            var buf = Encoding.UTF8.GetString(bytes.ToArray());
+
+            var image1 = JObject.Parse(buf)["substring"].ToString().Split(',')[1];
+            return Convert.FromBase64String(image1);
+        }
+    }
+}
---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using Nine_colored_deer_Sharp.Helper;
+using SharpAdbClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nine_colored_deer_Sharp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void checkAdbStatus()
+        {
+            if (!AdbServer.Instance.GetStatus().IsRunning)
+            {
+                AdbServer server = new AdbServer();
+                StartServerResult result = server.StartServer(Directory.GetCurrentDirectory() + "//adb//adb.exe", false);
+                if (result != StartServerResult.Started)
+                {
+                    Console.WriteLine("Can't start adb server");
+                    MessageBox.Show("adb服务启动失败，软件部分功能可能受限制！");
+                }
+            }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            checkAdbStatus();
+        }
+
+        private void SimplePanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btn_min_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btn_max_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+
+        }
+
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        kaiosHelper helper = new kaiosHelper();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var img = helper.getImage();
+
+            if (Directory.Exists(Directory.GetCurrentDirectory() + "\\screenshot") == false)
+            {
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\screenshot");
+
+            }
+
+            var imgname = Directory.GetCurrentDirectory() + "\\screenshot\\screenshot" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss") + ".png";
+
+            File.WriteAllBytes(imgname, img);
+        }
+        public static BitmapImage ByteArrayToBitmapImage(byte[] array)
+        {
+            using (var ms = new System.IO.MemoryStream(array))
+            {
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad; // here
+                image.StreamSource = ms;
+                image.EndInit();
+                image.Freeze();
+                return image;
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var img = helper.getImage();
+            var image = ByteArrayToBitmapImage(img);
+            this.image_screen.Source = image;
+
+        }
+    }
+}
